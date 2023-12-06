524a7a38e1501c14001ebde03b30f5f1abec9fed
r2r1 <grishaivan8@gamil.com>
2023-12-06 16:08:19 +0300

    test

--- informatics/lab4/task4/task4.py
+++ informatics/lab4/task4/task4.py
@@ -1,60 +1,108 @@
-json_file =  open("informatics/lab4/task4/task4.json", 'r', encoding="utf-8") 
 
-first_line = False
-flagNewBlock = False
-content = "---\n"
-check_tab= 0
+def parseTheObject(str,indent,pointer):
+    new_str=""
+    c=pointer
+    while (str[c]!="}"):
+        while(str[c]!="\""):
+            if(str[c]=="}"):
+                break
+            c+=1
 
-for line in json_file:
-    for c in "\'\"[]":
-            line = line.replace(c, '')
-    if ":{" in line :
-        line = line.replace(":{" , ': {')
-    if "," in line :
-        line = line.replace("," , ' ') 
+        if (str[c]=="}"):
+            break
         
-    new_line = ""
-    components = line.split()
-    
-    if components != []:  
-        if components[0] == '{' : 
-            check_tab += 1 
-            flagNewBlock = True 
+        res=parseTheField(str,indent+1,c)
+        new_str+="\n"+res[0]
+        new_str = new_str.replace("- }", "")
+        c=res[1]+1
+        
+        
+    return [new_str,c]
+
+def parseTheField(str,indent,pointer):
+    key,value="",""
+    c=pointer+1
+    while str[c]!="\"":
+        key+=str[c]
+        c+=1
+    c+=1
+    while str[c]==" " or str[c]==":":
+        c+=1
+
+
+    if(str[c]=="\""):
+        value+="\""
+        c+=1
+        while(str[c]!="\""):
+            value+=str[c]
+            c+=1
+        c+=1
+        value+="\""
+
+    elif (str[c]=="["):
+        res=parseTheArray(str,indent,c)
+        value="\n"+res[0]
+        c=res[1]
+
+    elif(str[c]=="{"):
+        res=parseTheObject(str,indent,c)
+        value=res[0]
+        c=res[1]
+
+    else:
+        while(str[c]!=" " and str[c]!="," and str[c]!="}"):
+            value+=str[c]
+            c+=1
+    return ["  "*indent+key+": "+value,c]
+
+def parseTheArray(str,indent,pointer):
+    new_str=""
+    c=pointer+1
+    while (str[c]!="]"):
+        while(str[c] in [" ",","," "]):
+            if(str[c]=="]"):
+                break
+            c+=1
             
-        if components.count("{") > 1 or components.count(":") > 1:
-            for i in  range(1, len(components)):
-                    if "{" == components[i]:
-                        check_tab +=1
-                        flagNewBlock == True
-                    else: 
-                        if (":" ==  components[i][-1]) and (":" !=  components[i+1][-1]) and ("{" not in components[i+1]):
-                            components[i+1] += "\n"
-                        elif ":" !=  components[i][-1] and ":" ==  components[i-1][-1] :
-                            True
-                        else:
-                            components[i] += "\n"
-                        if ("{" not in components[i])  and (flagNewBlock == True):
-                            new_line += ("\t"*(check_tab-1)) + components[i]
-                            flagNewBlock = False
-                        elif  not components[0] == '}' :
-                            new_line += ("\t"*(check_tab-1))+ components[i]
-                        
-                        if "{" in new_line:
-                            new_line =  new_line.replace("}", "\n")
-        else:   
-            key = components[0] 
-            value = ' '.join(components[1:])  
-            if "{" not in components[0]:
-                if (check_tab > 0) and (flagNewBlock == True) and not components[0] == '}' and ( (components[0] not in "}" ) and len(components) == 1) :
-                    new_line +=  ("\t"*check_tab)+ key + " " + value + "\n"
-                    flagNewBlock = False 
-                elif  not components[0] == '}' :
-                    new_line += ("\t"*check_tab) +  key + " " + value  + "\n"
-                if "{" in new_line:
-                    new_line =  new_line.replace("{", "\n")
-        if components[0] == '}':
-            check_tab -= 1  
-        if new_line != "":
-            content += new_line 
+        if (str[c]=="]"):
+            break
+        elif (str[c]=="{"):
+            res=parseTheObject(str,indent+1,c)
+        elif (str[c]=="["):
+            res=parseTheArray(str,indent+1,c)
+            res[0]="\n"+res[0]
+        elif (str[c]=="\""):
+            res=["",c+1]
+            while(str[res[1]]!="\""):
+                res[0]+=str[res[1]]
+                res[1]+=1
+        else:
+            res=["",c]
+            while(str[c] not in [" ",",","]"]):
+                res[0]+=str[c]
+                res[1]+=1
+                c+=1
+            
+        new_str+="  "*indent + "- "+res[0]+"\n"
+        c=res[1]
+        # print(res)
+        # print(c)
+        
+    return [new_str,c]
+
+
+f = open("informatics/lab4/task4/task4.json", 'r', encoding="utf-8") 
+
+
+jsonString=""
+
+for line in f:
+    tempLine=line[:-1]
+    jsonString+=tempLine
+
+yamlString="---"+parseTheObject(jsonString,-1,0)[0][:-5]
+
+
+print(yamlString) 
 with open("informatics/lab4/task4/task_4.yaml", 'w', encoding="utf-8") as f_out:
-   f_out.write(content)
+   f_out.write(yamlString)
